2fcfba8949430adb55db3bea657002ece1c8cd8b
Adrianne Nguyen <adriannenguyen07@gmail.com>
2023-08-09 10:31:31 -0700

    Initial Commit

---
+++ joyStemApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  joyStemApp
+//
+//  Created by Adrianne Nguyen on 8/9/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ joyStemApp/joyStemAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  joyStemAppApp.swift
+//  joyStemApp
+//
+//  Created by Adrianne Nguyen on 8/9/23.
+//
+
+import SwiftUI
+
+@main
+struct joyStemAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
